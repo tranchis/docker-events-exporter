8a4bdec413d6ed67163103ba9b2a12e8e7788b03
Sergio Alvarez-Napagao <tranchis@gmail.com>
2020-06-03 13:01:55 +0200

    Make it work without needing k8s

--- docker/events_notifier_prom.py
+++ docker/events_notifier_prom.py
@@ -22,47 +22,27 @@ from prometheus_client import start_http_server, Counter
 APP_NAME = "Docker events prometheus exporter"
 EVENTS = Counter('docker_events',
                  'Docker events',
-                 ['event', 'pod', 'env'])
-
-
-def print_timed(msg):
-    to_print = '{} [{}]: {}'.format(
-        datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-        'docker_events',
-        msg)
-    print(to_print)
+                 ['event', 'type', 'image', 'name'])
 
 
 def watch_events():
     client = docker.DockerClient(version='auto',
                                  base_url='unix://var/run/docker.sock')
     for event in client.events(decode=True):
-        attributes = event['Actor']['Attributes']
-        if event['Type'] == 'network':
-            continue
-        if 'io.kubernetes.docker.type' in attributes:
-            if attributes['io.kubernetes.container.name'] == 'POD':
-                continue
-            if event['status'].startswith(('exec_create', 'exec_detach')):
-                continue
-            msg = '{} on {} ({})'.format(
-                event['status'].strip(),
-                attributes['io.kubernetes.pod.name'],
-                attributes['io.kubernetes.pod.namespace'])
-            print_timed(msg)
-            pod = attributes['io.kubernetes.container.name']
-            if event['status'] == 'oom':
-                pod = attributes['io.kubernetes.pod.name']
-            EVENTS.labels(event=event['status'].strip(),
-                          pod=pod,
-                          env=attributes['io.kubernetes.pod.namespace']).inc()
+        type_event = event['Type']
+        try:
+            actor = event['Actor']
+            attributes = actor['Attributes']
+            event = event['status'].strip()
+            EVENTS.labels(event=event, type=type_event, image=attributes['image'], name=attributes['name']).inc()
+        except Exception as e:
+            print(event)
+            pass
 
 
 if __name__ == '__main__':
-    print_timed('Start prometheus client on port 9000')
     start_http_server(9000, addr='0.0.0.0')
     try:
-        print_timed('Watch docker events')
         watch_events()
     except docker.errors.APIError:
         pass
